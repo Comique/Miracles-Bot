7eb62c92e28ea6292b7b7f631287899515c87dca
Martin Chuang <martin.chuang88@outlook.com>
2020-08-14 16:22:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ client.on('message', msg => {
                     messageReaction.react(reactEmoji);
                     messageReaction.awaitReactions((args, user) => {
                         if (args._emoji.name === reactEmoji && !user.bot) {
-                            if (i > max - 1)
+                            if (i > max - 1 && !waitlist)
                                 mess = mess + '\n__Waitlist:__'
-                                waitlist = 1;
+                                waitlist = 69;
                             else if (i == 0)
                                 mess = mess + '\n__Registered:__ '
                             if (!list.includes(user.username)) {
