68e2670dec3b036e16bc433137444bd709dc2283
Martin Chuang <martin.chuang88@outlook.com>
2020-08-15 19:40:25 -0400

    update

--- handlers/carry.js
+++ handlers/carry.js
@@ -1,9 +1,18 @@
 const Discord = require('discord.js');
+
 const reactEmoji = '🤍';
+
+var users = new Array();
+var max;
+var send;
 
 module.exports = {
     name: 'carry',
     description: 'set up a carry post',
+    users: users,
+    max: max,
+    send: send,
+    updateList: updateList,
     execute(msg, args) {
 
         msg.delete();
@@ -15,24 +24,22 @@ module.exports = {
             var time = args[2];
             var ampm = args[3].toUpperCase();
             var tz = args[4].toUpperCase();
-            var max = args[5];
+            max = args[5];
 
-            const message = '<@' + msg.author + '> is carrying __**' + boss + '**__ on __**' + date + ' ' + time + ' ' + ampm + ' (' + tz + ')**__'
+            send = '<@' + msg.author + '> is carrying __**' + boss + '**__ on __**' + date + ' ' + time + ' ' + ampm + ' (' + tz + ')**__'
                 + '\nThe party will be meeting at CH20 Root Abyss'
                 + '\n__**' + max + '**__ slots are available in the party.'
                 + '\n**Reminder that we will be checking for contribution, please only react if you have the required amount.**'
                 + '\nReact to this message to reserve a spot.';
 
-            var users = new Array();
-
             msg.channel
-                .send({ embed: { color: 0x81D8D0, description: message } })
+                .send({ embed: { color: 0x81D8D0, description: send } })
                 .then((sentMessage) => {
                     sentMessage.react(reactEmoji);
                     sentMessage.awaitReactions((reaction, user) => {
                         if (reaction._emoji.name === reactEmoji && !user.bot) {
                             users.push(user);
-                            sentMessage.edit({ embed: { color: 0x81D8D0, description: updateList(message, users, max) } });
+                            updateList(users, sentMessage);
                         }
                     });
                 });
@@ -41,7 +48,7 @@ module.exports = {
 
             msg.author.send({
                 embed: {
-                    color: 0x000000, description: "Invalid arguments."
+                    color: 0x81D8D0, description: "Invalid arguments."
                         + "\nCorrect usage: ;carry [boss] [mm/dd] [xx:xx] [am/pm] [timezone] [slots]"
                 }
             });
@@ -50,16 +57,16 @@ module.exports = {
     }
 }
 
-function updateList(message, users, max) {
+function updateList(users, sentMessage) {
 
-    var newMessage = message;
+    let newMessage = send;
 
     if (users.length <= max)
         newMessage += "\nRegistered (" + users.length + "/" + max + "):";
     else
         newMessage += "\nRegistered (" + max + "/" + max + "):";
 
-    for (var i = 1; i <= users.length; i++) {
+    for (let i = 1; i <= users.length; i++) {
 
         if (i - 1 == max)
             newMessage += "\nWaitlist: ";
@@ -68,6 +75,6 @@ function updateList(message, users, max) {
 
     }
 
-    return newMessage;
+    sentMessage.edit({ embed: { color: 0x81D8D0, description: newMessage } });
 
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
+const carry = require('./handlers/carry.js');
 const client = new Discord.Client();
 
-const prefix = '!';
+const prefix = ';';
 
 const fs = require('fs');
+const { users } = require('./handlers/carry');
 client.handlers = new Discord.Collection();
 
 const handlerFiles = fs.readdirSync('./handlers/').filter(file => file.endsWith('.js'));
@@ -14,6 +16,18 @@ for (const file of handlerFiles) {
 
 client.on('ready', () => {
     console.log('Test bot online.');
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+    if (user.bot) return;
+    if (reaction.message.author.bot && reaction.emoji.name === '🤍') {
+        console.log(users);
+        for (let i = 0; i < users.length; i++)
+            if (users[i] == user)
+                users.splice(i, 1);
+        console.log(users);
+        carry.updateList(users, reaction.message);
+    }
 });
 
 client.on('message', msg => {
